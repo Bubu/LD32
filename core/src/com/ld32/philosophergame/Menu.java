0680753b56ce32593877a2c6c4ec514ef599139b
Marcus Hoffmann <bubuiic@aol.com>
2015-04-19 01:22:22 +0200

    Menu work

--- core/src/com/ld32/philosophergame/Menu.java
+++ core/src/com/ld32/philosophergame/Menu.java
@@ -1,20 +1,20 @@
 package com.ld32.philosophergame;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class Menu extends Table {
+	KeyTextButton[] entries;
 	
 
 	public Menu() {
-		// TODO Auto-generated constructor stub
+		entries = new KeyTextButton[4];
 	}
 
-	public Menu(Skin skin) {
-		super(skin);
-		// TODO Auto-generated constructor stub
-	}
 	public void add(KeyTextButton entry){
-		this.add(entry);
+		if(entries.length>2){
+			this.row();
+			entries[entries.length-1] = entry;
+		}
+			this.add(entry);
 	}
 }
--- core/src/com/ld32/philosophergame/Opponent.java
+++ core/src/com/ld32/philosophergame/Opponent.java
@@ -1,15 +1,9 @@
 package com.ld32.philosophergame;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.utils.Scaling;
 
-public class Opponent extends Image {
+public class Opponent {
 
-	public Opponent(Drawable drawable, Scaling scaling) {
-		super(drawable, scaling);
-		// TODO Auto-generated constructor stub
+	public Opponent() {
 	}
-
 
 }
