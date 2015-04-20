1cacb6a20a3a4eb412c367cb193e5dd679571046
Marcus Hoffmann <bubuiic@aol.com>
2015-04-20 23:55:42 +0200

    added quoates

--- core/src/com/ld32/philosophergame/Attack.java
+++ core/src/com/ld32/philosophergame/Attack.java
@@ -100,6 +100,22 @@ public class Attack {
 		return new Attack("Socratic Method", 1, 0, 0, 0.5f, 1, messages);
 	}
 	
+	public static Attack PlatonicLove(){
+		String[] messages = new String[3];
+		messages[0] = "I would love to see you"+n+"handcuffed in a dark cave!";
+		messages[1] = "I like the idealised version of you.";
+		messages[2] = "How does it feel to be ruled"+n+"by the philosopher king?";
+		return new Attack("Platonic Love", 1, 0, 0, 0.5f, 1, messages);
+	}	
+	
+	public static Attack AristotelianLogic(){
+		String[] messages = new String[3];
+		messages[0] = "All living things are beautiful."+n+"I am a living thing."+n+"Therfore I am beautiful. Oh, yeah!";
+		messages[1] = "Every non-Socrates against a"+n+"non-non-Socrates will be non-winning.";
+		messages[2] = "One man's modus ponens is another man's modus tollens.";
+		return new Attack("Aristotelian Logic", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
 	// Attack Schopenhauer
 	
 	public static Attack Pessimism(){
@@ -126,22 +142,137 @@ public class Attack {
 		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
 	}
 	
-	public static Attack PlatonicLove(){
-		String[] messages = new String[3];
-		messages[0] = "I would love to see you"+n+"handcuffed in a dark cave!";
-		messages[1] = "I like the idealised version of you.";
-		messages[2] = "How does it feel to be ruled"+n+"by the philosopher king?";
-		return new Attack("Platonic Love", 1, 0, 0, 0.5f, 1, messages);
-	}	
+	// Wittgenstein Attacks
 	
-	public static Attack AristotelianLogic(){
+	public static Attack LogicalAtomism(){
 		String[] messages = new String[3];
-		messages[0] = "All living things are beautiful."+n+"I am a living thing."+n+"Therfore I am beautiful. Oh, yeah!";
-		messages[1] = "Every non-Socrates against a"+n+"non-non-Socrates will be non-winning.";
-		messages[2] = "One man's modus ponens is another man's modus tollens.";
-		return new Attack("Aristotelian Logic", 1, 0, 0, 0.5f, 1, messages);
+		messages[0] = "Time to diconstruct you!";
+		messages[1] = "Some of your statements are senseless, some are nonsensical."+n+"The rest is simply wrong.";
+		messages[2] = "Whereof one cannot speak, thereof one must be silent.";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
 	}
 	
+	public static Attack LanguageGames(){
+		String[] messages = new String[3];
+		messages[0] = "What is the difference between a duck?";
+		messages[1] = "How are a chicken and a grape alike?"+n+"They are both purple... except for the chicken.";
+		messages[2] = "We can not say that you am the worst of all times,"+n+"because we have no concept of 'of all times'.";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack BeatleInABox(){
+		String[] messages = new String[3];
+		messages[0] = "My beatle is much bigger than yours.";
+		messages[1] = "In my world I am the winner. In your world we may even tie!";
+		messages[2] = "My box seems to be bigger on the inside!";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	// Hobbes Attacks
+	
+	public static Attack SocialContract(){
+		String[] messages = new String[3];
+		messages[0] = "I'm making you an offer you can not refuse.";
+		messages[1] = "This is our only chance to escape the state of nature.";
+		messages[2] = "Just accept the terms and conditions and sign here!";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack AllAgainstAll(){
+		String[] messages = new String[3];
+		messages[0] = "The condition of man... is a condition of"+n+"war of everyone against everyone.";
+		messages[1] = "Life is nasty, brutish, and short";
+		messages[2] = "Men look not at the greatnesse of the evill past, but the greatnesse of the good to follow!";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack SummonLeviathan(){
+		String[] messages = new String[3];
+		messages[0] = "There is no power on earth to be compared to the Leviathan.";
+		messages[1] = "I broke my back lifting the Leviathan to heaven.";
+		messages[2] = "Now I am about to take my last voyage, a great leap in the dark";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	// Freud Attacks
+	
+	public static Attack Tabu(){
+		String[] messages = new String[3];
+		messages[0] = "Silence, you fool!";
+		messages[1] = "It is generally assumed that the taboo is older"+n+"than the gods and goes back to the pre-religious age.";
+		messages[2] = "The mind should develop a blind spot whenever a dangerous"+n+"thought presented itself. The process should be automatic, instinctive. ";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack SexualRepression(){
+		String[] messages = new String[3];
+		messages[0] = "Your aggression seems to come from an erotic infatuation to your grandmother.";
+		messages[1] = "Sometimes a cigar is just a cigar.";
+		messages[2] = "I sense a slight castration anxiety. Don't worry this is natural.";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack SummonSuperEgo(){
+		String[] messages = new String[3];
+		messages[0] = "The ego is not master in its own house.";
+		messages[1] = "The super-ego is never so inexorable, so impersonal, so independent"+n+"of its emotional origins as we require it to be in men";
+		messages[2] = "My ego, my super-ego and I, that's all I got in the end."+n+"That's what I found out and there ain't no need to cry.";
+		return new Attack("Summon Ubermensch", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	// Kant Attacks
+	
+	public static Attack PureReason(){
+		String[] messages = new String[3];
+		messages[0] = "Silence, you fool!";
+		messages[1] = "It is generally assumed that the taboo is older"+n+"than the gods and goes back to the pre-religious age.";
+		messages[2] = "The mind should develop a blind spot whenever a dangerous"+n+"thought presented itself. The process should be automatic, instinctive. ";
+		return new Attack("Pure Reson", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack CategoricalImperative(){
+		String[] messages = new String[3];
+		messages[0] = "Act only on that maxim through which you can at the"+n+"same time will that it should become a universal law";
+		messages[1] = "Call me the categorical imperator!";
+		messages[2] = "By a lie, a man... annihilates his dignity as a man.";
+		return new Attack("Categorical Imperative", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack Enlightenment(){
+		String[] messages = new String[3];
+		messages[0] = "Have the courage to use your own reason --"+n+"That is the motto of enlightenment";
+		messages[1] = "Knowing others is wisdom, knowing yourself is Enlightenment.";
+		messages[2] = "It must be difficult to work yourself out of the immaturity which has become almost natural for you.";
+		return new Attack("Enlightenment", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	// Kierkegard Attacks
+	
+	public static Attack Angst(){
+		String[] messages = new String[3];
+		messages[0] = "We can easily forgive a child who is afraid of the dark;"+n+"the real tragedy of life is when men are afraid of the light.";
+		messages[1] = "Oh, my sweet summer child, what do you know of angst?";
+		messages[2] = "Anxiety is an alien power which lays hold of the individual;"+n+"and yet one cannot tear oneself away, not has a will to do so;"+n+"for one fears, but what one fears one desires";
+		return new Attack("Pure Reson", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack ExistentialDespair(){
+		String[] messages = new String[3];
+		messages[0] = "Life can only be understood backwards; but it must be lived forwards.";
+		messages[1] = "In a few hundred years no one will know"+n+"who you are or that you ever existed.";
+		messages[2] = "The most painful state of being is remembering the future,"+n+"particularly the one you'll never have.";
+		return new Attack("Existential Despair", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+	public static Attack SummonKnightOfFaith(){
+		String[] messages = new String[3];
+		messages[0] = "The true knight of faith is a witness, never a teacher, and therein lies his deep humanity.";
+		messages[1] = "Faith consists in believing when it is beyond the power of reason to believe.";
+		messages[2] = "It must be difficult to work yourself out of the immaturity which has become almost natural for you.";
+		return new Attack("Summon Knight of Faith", 1, 0, 0, 0.5f, 1, messages);
+	}
+	
+
 	public Attack(String name, int coolDown, float hpDamage, float sanityDamage, float bonus, float malus, String[] messages) {
 		this.name = name;
 		this.coolDown = coolDown;
--- core/src/com/ld32/philosophergame/Philosopher.java
+++ core/src/com/ld32/philosophergame/Philosopher.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
 
 public class Philosopher {
-	public static enum Philosophers {Socrates, Descartes, Nietzsche, Schopenhauer, Kirkegaard, Hobbes, Freud, Kant, Wittgenstein};
+	public static enum Philosophers {Socrates, Descartes, Nietzsche, Schopenhauer, Kierkegaard, Hobbes, Freud, Kant, Wittgenstein};
 	int maxhp;
 	int currenthp;
 	int sanity;
@@ -18,7 +18,7 @@ public class Philosopher {
 	int currentSanity;
 
 	static String n = System.getProperty("line.separator");
-	
+
 	public static Philosopher createPhilosopher(Philosophers phil, boolean isOpponent){
 		switch(phil){
 		case Descartes:
@@ -28,49 +28,49 @@ public class Philosopher {
 		case Nietzsche:
 			return Nietzsche(isOpponent);
 		case Schopenhauer:
-			return Nietzsche(isOpponent);
-		case Kirkegaard:
-			return Nietzsche(isOpponent);
+			return Schopenhauer(isOpponent);
+		case Kierkegaard:
+			return Kierkegaard(isOpponent);
 		case Hobbes:
-			return Nietzsche(isOpponent);
+			return Hobbes(isOpponent);
 		case Freud:
-			return Nietzsche(isOpponent);
+			return Freud(isOpponent);
 		case Kant:
-			return Nietzsche(isOpponent);
+			return Kant(isOpponent);
 		case Wittgenstein:
-			return Nietzsche(isOpponent);
+			return Wittgenstein(isOpponent);
 		default:
 			return null;
 		}
 	}
-	
+
 	public static Philosopher Nietzsche(boolean isOpponent){	
 		Philosopher p = new Philosopher("Nietzsche", 100,20,Ressources.Nietzsche(),isOpponent);
 		p.attacks[3] = Attack.EternalReturn();
 		p.attacks[4] = Attack.WilltoPower();
 		p.attacks[5] = Attack.SummonUbermensch();
-		
+
 		p.phrases[0] = "You shall be overcome.";
 		p.phrases[1] = "I perish ... and in perishing ... I triumph!";
 		p.phrases[2] = "You found me before you found yourself.";
 		return p;
 	}
 	public static Philosopher Descartes(boolean isOpponent){
-		
+
 		Philosopher p = new Philosopher("Descartes", 100,20,Ressources.Descartes(),isOpponent);
 		p.attacks[3] = Attack.CartesianDualism();
 		p.attacks[4] = Attack.DeusDeceptor();
 		p.attacks[5] = Attack.CogitoErgoSum();
-		
+
 		p.phrases[0] = "I think -- therefore I kick ass!";
 		p.phrases[1] = "I am indeed amazed when I consider how weak"+n+"my mind is and how prone to error.";
 		p.phrases[2] = "Perfect men like perfect numbers are very rare."+n+"You are not one of them.";
-		
+
 		return p;
 	}
-	
-public static Philosopher Socrates(boolean isOpponent){
-		
+
+	public static Philosopher Socrates(boolean isOpponent){
+
 		Philosopher p = new Philosopher("Socrates", 100,20,Ressources.Socrates(),isOpponent);
 		p.attacks[3] = Attack.SocraticMethod();
 		p.attacks[4] = Attack.PlatonicLove();
@@ -78,59 +78,59 @@ public static Philosopher Socrates(boolean isOpponent){
 		return p;
 	}
 
-public static Philosopher Schopenhauer(boolean isOpponent){
-	
-	Philosopher p = new Philosopher("Schopenhauer", 100,20,Ressources.Schopenhauer(),isOpponent);
-	p.attacks[3] = Attack.Pessimism();
-	p.attacks[4] = Attack.HedgehogDilemma();
-	p.attacks[5] = Attack.LifeIsSuffering();
-	return p;
-}
+	public static Philosopher Schopenhauer(boolean isOpponent){
 
-public static Philosopher Wittgenstein(boolean isOpponent){
-	
-Philosopher p = new Philosopher("Wittgenstein", 100,20,Ressources.Wittgenstein(),isOpponent);
-p.attacks[3] = Attack.LogicalProposition();
-p.attacks[4] = Attack.LanguageGames();
-p.attacks[5] = Attack.BeatleInABox();
-return p;
-}
+		Philosopher p = new Philosopher("Schopenhauer", 100,20,Ressources.Schopenhauer(),isOpponent);
+		p.attacks[3] = Attack.Pessimism();
+		p.attacks[4] = Attack.HedgehogDilemma();
+		p.attacks[5] = Attack.LifeIsSuffering();
+		return p;
+	}
 
-public static Philosopher Hobbes(boolean isOpponent){
-	
-Philosopher p = new Philosopher("Hobbes", 100,20,Ressources.Hobbes(),isOpponent);
-p.attacks[3] = Attack.AllAgainstAll();
-p.attacks[4] = Attack.SocialContract();
-p.attacks[5] = Attack.SummonLeviathan();
-return p;
-}
+	public static Philosopher Wittgenstein(boolean isOpponent){
 
-public static Philosopher Freud(boolean isOpponent){
-	
-Philosopher p = new Philosopher("Freud", 100,20,Ressources.Freud(),isOpponent);
-p.attacks[3] = Attack.Tabu();
-p.attacks[4] = Attack.SexualRepression();
-p.attacks[5] = Attack.SummonSuperEgo();
-return p;
-}
+		Philosopher p = new Philosopher("Wittgenstein", 100,20,Ressources.Wittgenstein(),isOpponent);
+		p.attacks[3] = Attack.LogicalAtomism();
+		p.attacks[4] = Attack.LanguageGames();
+		p.attacks[5] = Attack.BeatleInABox();
+		return p;
+	}
 
-public static Philosopher Kant(boolean isOpponent){
+	public static Philosopher Hobbes(boolean isOpponent){
 
-Philosopher p = new Philosopher("Kant", 100,20,Ressources.Kant(),isOpponent);
-p.attacks[3] = Attack.PureReason();
-p.attacks[4] = Attack.CathegorialImperativ();
-p.attacks[5] = Attack.Enlightenment();
-return p;
-}
+		Philosopher p = new Philosopher("Hobbes", 100,20,Ressources.Hobbes(),isOpponent);
+		p.attacks[3] = Attack.AllAgainstAll();
+		p.attacks[4] = Attack.SocialContract();
+		p.attacks[5] = Attack.SummonLeviathan();
+		return p;
+	}
 
-public static Philosopher Kierkegaard(boolean isOpponent){
+	public static Philosopher Freud(boolean isOpponent){
 
-Philosopher p = new Philosopher("Kierkegaard", 100,20,Ressources.Kierkegaard(),isOpponent);
-p.attacks[3] = Attack.Angst();
-p.attacks[4] = Attack.ExistentialDespair();
-p.attacks[5] = Attack.summonKnightOfFaith();
-return p;
-}
+		Philosopher p = new Philosopher("Freud", 100,20,Ressources.Freud(),isOpponent);
+		p.attacks[3] = Attack.Tabu();
+		p.attacks[4] = Attack.SexualRepression();
+		p.attacks[5] = Attack.SummonSuperEgo();
+		return p;
+	}
+
+	public static Philosopher Kant(boolean isOpponent){
+
+		Philosopher p = new Philosopher("Kant", 100,20,Ressources.Kant(),isOpponent);
+		p.attacks[3] = Attack.PureReason();
+		p.attacks[4] = Attack.CategoricalImperative();
+		p.attacks[5] = Attack.Enlightenment();
+		return p;
+	}
+
+	public static Philosopher Kierkegaard(boolean isOpponent){
+
+		Philosopher p = new Philosopher("Kierkegaard", 100,20,Ressources.Kierkegaard(),isOpponent);
+		p.attacks[3] = Attack.Angst();
+		p.attacks[4] = Attack.ExistentialDespair();
+		p.attacks[5] = Attack.SummonKnightOfFaith();
+		return p;
+	}
 
 	public Philosopher(String name, int maxhp, int sanity, TextureRegion texture, boolean isOpponent) {
 		this.name = name;
@@ -163,9 +163,9 @@ return p;
 		Attack attack = attacks[Ressources.Rand().nextInt(attacks.length)];
 		return attack;
 	}
-	
+
 	public static Philosopher getRandom() {
-		
+
 		return null;
 	}
 
