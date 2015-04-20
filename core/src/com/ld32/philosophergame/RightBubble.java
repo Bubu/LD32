b249d065d5660c4e7e87acd4f7de2f81014a3c3d
Marcus Hoffmann <bubuiic@aol.com>
2015-04-20 13:41:20 +0200

    bubbles

---
+++ core/src/com/ld32/philosophergame/RightBubble.java
@@ -0,0 +1,13 @@
+package com.ld32.philosophergame;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class RightBubble extends Label {
+
+	public RightBubble(CharSequence text) {
+		super(text, Ressources.BubbleStyleRight());
+		this.setPosition(160, 250);
+	}
+	
+
+}
